f81f6d2ec479391007f03404d862e1ccb8653abf
Odion <odionfiona@gmail.com>
2024-11-05 01:22:49 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.tsx 	modified:   src/form.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,24 @@
 // import NxWelcome from './nx-welcome';
+// import {useState} from "react"
+
+import Form from "../form"
 
 export function App() {
+
+  // const [todo, setTodo] = useSate ([])
+
   return (
-    <div>
-      {/* <NxWelcome title="org" /> */}
-      <h1>hello </h1>
-    </div>
+    <>
+      <div className="p-[200px]">
+        {/* <NxWelcome title="org" /> */}
+        <h1 className="text-[50px] font-bold">Todo List </h1>
+
+        {/* form component */}
+        <div className="py-10">
+          <Form />
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/form.tsx
+++ src/form.tsx
@@ -1,14 +1,30 @@
-import styled from 'tailwind';
 
-const StyledForm = styled.div`
-  color: pink;
-`;
+import { IoAddCircle } from 'react-icons/io5';
+
 
 export function Form() {
   return (
-    <StyledForm>
-      <h1>Welcome to Form!</h1>
-    </StyledForm>
+    <>
+      <div>
+        <form className=" flex gap-[5px]">
+          <div className="text-[30px] text-[#8c11ce]">
+            <IoAddCircle />
+          </div>
+
+          <div className=" flex justify-between gap-[20px] ">
+            <input
+              className="px-5 w-[700px]"
+              type="text"
+              placeholder="Memorize the dictionary"
+              required
+            />
+            <button className="px-6 py-2 bg-[#8c11ce] text-[#fff] rounded-lg">
+              Add Item
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
 
